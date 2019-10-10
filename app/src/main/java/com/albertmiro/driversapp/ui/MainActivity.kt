9810c4534583523658a14bccf028666d204bbf9b
AlbertM <info@albertmiro.com>
2019-10-10 19:17:57 +0200

    Moved some methods to BaseMapFragment, and some name renaming

--- app/src/main/java/com/albertmiro/driversapp/ui/MainActivity.kt
+++ app/src/main/java/com/albertmiro/driversapp/ui/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        loadTaxiListFragment()
+        loadVehiclesListFragment()
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/com/albertmiro/driversapp/ui/taxis/adapter/VehicleItemView.kt
@@ -0,0 +1,21 @@
+package com.albertmiro.driversapp.ui.taxis.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.RelativeLayout
+import com.albertmiro.domain.domain.Vehicle
+import com.albertmiro.driversapp.R
+import com.albertmiro.driversapp.ui.bindTaxi
+import kotlinx.android.synthetic.main.item_vehicle.view.*
+
+
+class VehicleItemView constructor(context: Context) : RelativeLayout(context) {
+    init {
+        LayoutInflater.from(context).inflate(R.layout.item_vehicle, this, true)
+    }
+
+    fun bind(vehicle: Vehicle) {
+        bindTaxi(vehicle, vehicleHeader, vehicleDescription, vehicleImage)
+    }
+
+}
